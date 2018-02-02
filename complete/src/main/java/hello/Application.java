9302a367067a8086c539fe1210639d5861df0ccc
Pedro Folim <pedrofolim@gmail.com>
2018-02-02 13:02:23 -0200

    äjuste para deserializar json para request

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,15 +1,21 @@
 
 package hello;
 
+import java.io.File;
+import java.io.FileReader;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
+import org.json.simple.parser.JSONParser;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
@@ -34,7 +40,7 @@ public class Application {
 			// configure mapper, if necessary, then create schema generator
 			JsonSchemaGenerator schemaGen = new JsonSchemaGenerator(mapper);
 			JsonSchema schema = schemaGen.generateSchema(GetQuote.class);
-			System.out.println(mapper.writeValueAsString(schema));
+			System.out.println(String.format("Request JSON generate ",mapper.writeValueAsString(schema)));
 		};
     }
 	
@@ -47,20 +53,30 @@ public class Application {
 			// configure mapper, if necessary, then create schema generator
 			JsonSchemaGenerator schemaGen = new JsonSchemaGenerator(mapper);
 			JsonSchema schema = schemaGen.generateSchema(GetQuoteResponse.class);
-			System.out.println(mapper.writeValueAsString(schema));
+			System.out.println(String.format("Response JSON generate ",mapper.writeValueAsString(schema)));
 		};
     }
 
 	@Bean
 	CommandLineRunner lookup(SoapClient soapClient) {
 		return args -> {
+			
+			//urls
 			String wsdl = "http://www.webservicex.com/stockquote.asmx";
 			String callback = "http://www.webserviceX.NET/GetQuote";
 			
+			//context flow
 			FlowContext context = new FlowContext();
 			context.getSteps().add(new Step("Obter Stoque","MSFT"));
-			
-			String response = soapClient.call(wsdl, callback, context);
+						
+			//convert json to Object
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+			JaxbAnnotationModule module = new JaxbAnnotationModule();
+			mapper.registerModule(module);
+			GetQuote request = mapper.readValue(new File("/Users/folim/workspace/sysmap/pocs/gs-consuming-web-service/complete/src/main/resources/TaskSoap/taskSoapRequestTemplate.json"), GetQuote.class);
+			request.setSymbol("MSFT");
+			String response = soapClient.call(wsdl, callback, request);
 			
 			System.err.println(response);
 		};
--- complete/src/main/java/hello/SoapClient.java
+++ complete/src/main/java/hello/SoapClient.java
@@ -3,23 +3,18 @@ package hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
-import hello.wsdl.GetQuote;
 import hello.wsdl.GetQuoteResponse;
 
 public class SoapClient extends WebServiceGatewaySupport {
 
 	private static final Logger log = LoggerFactory.getLogger(SoapClient.class);
 
-	public String call(String wsdl,String callback, FlowContext context) {
+	public String call(String wsdl,String callback, Object request) {
 
-		GetQuote request = new GetQuote();
-		request.setSymbol("");
-
-		log.info("Requesting SoapClient for " + context.toString());
+		log.info("Requesting SoapClient for " + request.toString());
 
 		GetQuoteResponse response = (GetQuoteResponse) getWebServiceTemplate()
 				.marshalSendAndReceive(wsdl,
